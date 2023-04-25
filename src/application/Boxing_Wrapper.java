5145d828c321982ac7cc563e720c54c7dc7f8293
Maria Eduarda <mariaeduardasilv916@gmail.com>
2023-04-25 10:53:56 -0300

    Salvando projeto

---
+++ src/application/Boxing_Wrapper.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Boxing_Wrapper {
+
+	public static void main(String[] args) {
+		
+		//boxing e unboxing 
+		
+		int x = 20;
+		
+		Integer obj = x;
+		
+		System.out.println(obj);
+		
+		int y = obj * 2;
+		
+		System.out.println(y);
+		
+	}
+
+}
+
